5d240411d97729b76a502684e131b0e80e8fc299
Le-Yo <deoleyo@gmail.com>
2023-05-22 13:14:07 -0400

    sports links

---
+++ routes/EventRoute.js
@@ -0,0 +1,27 @@
+
+// const express = require('express');
+// const Event = require('../models/Event');
+// const router = express.Router();
+
+// router.post('/', async (req, res, next) => {
+//   let event = new Event({
+//     event: req.body.event,
+//     date: req.body.date,
+//     contact: req.body.contact,
+//     address: req.body.address,
+//     markdown: req.body.markdown,
+//   });
+//   try {
+//     Event = await event.save();
+//     res.redirect('/');
+//   } catch (e) {
+//     console.warn(e);
+//     res.render(`event`, { event: event });
+//   }
+  
+// });
+
+
+
+// module.exports = router;
+
